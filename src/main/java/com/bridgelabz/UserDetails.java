38f0ad27ce7735d50158c9a46b000bed0e2b4da8
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-14 23:17:45 +0530

    checking Emails is valid or invalid

--- src/main/java/com/bridgelabz/UserDetails.java
+++ src/main/java/com/bridgelabz/UserDetails.java
@@ -5,11 +5,13 @@ import java.util.regex.Pattern;
 
 public class UserDetails {
 	public String input;
+	SampleEmails testSampleEmail = new SampleEmails();
+
 	public boolean firstName(String firstName) {
 		input = "([A-Z]{1}[a-z]{2,})";
 		Pattern p = Pattern.compile(input);
 		Matcher m = p.matcher(firstName);
-		if (m.matches()==true) {
+		if (m.matches() == true) {
 			System.out.println("Valid Input");
 		} else {
 			System.out.println("Sorry!! InValid Input");
@@ -17,11 +19,12 @@ public class UserDetails {
 		}
 		return m.matches();
 	}
+
 	public boolean lastName(String lastName) {
 		input = "([A-Z]{1}[a-z]{2,})";
 		Pattern p = Pattern.compile(input);
 		Matcher m = p.matcher(lastName);
-		if (m.matches()==true) {
+		if (m.matches() == true) {
 			System.out.println("Valid Input");
 		} else {
 			System.out.println("Sorry!! InValid Input");
@@ -29,13 +32,14 @@ public class UserDetails {
 		}
 		return m.matches();
 	}
+
 	public boolean email(String email) {
-		input = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}$";
+		input = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
 
 		// Pattern p = Pattern.complie("((?=.*\\d).{8,20})");
 		Pattern p = Pattern.compile(input);
 		Matcher m = p.matcher(email);
-		if (m.matches()==true) {
+		if (m.matches() == true) {
 			System.out.println("Valid Input");
 		} else {
 			System.out.println("Sorry!! InValid Input");
@@ -43,16 +47,47 @@ public class UserDetails {
 		}
 		return m.matches();
 	}
+
 	public boolean pwd(String pwd) {
 		input = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]*[\\@\\#\\^][a-zA-Z0-9]*$";
 
 		// Pattern p = Pattern.complie("((?=.*\\d).{8,20})");
 		Pattern p = Pattern.compile(input);
 		Matcher m = p.matcher(pwd);
-		if (m.matches()==true) {
+		if (m.matches() == true) {
 			System.out.println("Valid Input");
 		} else {
 			System.out.println("Sorry!! InValid Input");
+			System.out.println("Try valid Input");
+		}
+		return m.matches();
+	}
+
+	public boolean phoneNum(String number) {
+		input = "^([0-9]{2}[\\s]){1}[0-9]{9,10}$";
+
+		Pattern p = Pattern.compile(input);
+		Matcher m = p.matcher(number);
+		System.out.println(m.matches());
+		if(m.matches()) {
+			System.out.println("Phone NUmber is Valid");
+		}
+		else {
+			System.out.println("Phone NUmber is Not Valid");
+
+		}
+		return m.matches();
+	}
+
+	public boolean sampleEmails(String email) {
+
+		input = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+		Pattern p = Pattern.compile(input);
+		Matcher m = p.matcher(email);
+		if (m.matches() == true) {
+			System.out.println(testSampleEmail.testForValidEmails());
+		} else {
+			System.out.println(testSampleEmail.testForNonValidEmails());
 			System.out.println("Try valid Input");
 		}
 		return m.matches();
--- src/test/java/com/bridgelabz/UserDetailsTest.java
+++ src/test/java/com/bridgelabz/UserDetailsTest.java
@@ -8,7 +8,7 @@ public class UserDetailsTest {
 	// @Mock
 	// private UserDetails details;
 	UserDetails details = new UserDetails();
-
+	SampleEmails se=new SampleEmails();
 	/**
 	 * We have created this Test case to check if the First name is proper & if
 	 * passes the regex pattern. The assertTrue will check if the result is true and
@@ -72,9 +72,10 @@ public class UserDetailsTest {
 		boolean actualResult1 = details.email("sir@@bl.co.in");
 		Assert.assertFalse(actualResult1);
 	}
+
 	/**
-	 * Test case to check if the password is valid then result is true and the it will
-	 * pass the test.
+	 * Test case to check if the password is valid then result is true and the it
+	 * will pass the test.
 	 */
 	@Test
 	public void isValidPassword() {
@@ -83,12 +84,50 @@ public class UserDetailsTest {
 	}
 
 	/**
-	 * Test case to check if the password is not valid then result is true and the it
-	 * will fails the test.
+	 * Test case to check if the password is not valid then result is true and the
+	 * it will fails the test.
 	 */
 	@Test
 	public void isInvalidPassword() {
 		boolean actualResult1 = details.pwd("Password@#123");
 		Assert.assertFalse(actualResult1);
+	}
+
+	/**
+	 * Test case to check if the PhoneNo is valid then result is true and the it
+	 * will pass the test.
+	 */
+	@Test
+	public void isValidPhoneNo() {
+		boolean actualResult = details.pwd("91 9866349586");
+		Assert.assertTrue(actualResult);
+	}
+
+	/**
+	 * Test case to check if the PhoneNo is not valid then result is true and the
+	 * it will fails the test.
+	 */
+	@Test
+	public void isInvalidPhoneNo() {
+		boolean actualResult1 = details.pwd("919866795861");
+		Assert.assertFalse(actualResult1);
+	}
+	/**
+	 * Test method to check  sample emails are valid
+	 */
+	@Test
+	public void checkValidEmails()
+	{
+		boolean result = details.sampleEmails("abc111@abc.com");
+		Assert.assertTrue(result);
+	}
+	/**
+	 * Test method to check sample emails are invalid
+	 */
+	@Test
+	public void checkInValidEmails()
+	{
+		boolean result1 = details.sampleEmails("abc");
+		Assert.assertFalse(result1);
 	}
 }
