bd0ec47e984528a1ea791d7d339d4c59c48811d3
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-03-14 22:17:58 +0530

    checking emails are valid or invalid

--- src/main/java/com/bridgelabz/UserDetails.java
+++ src/main/java/com/bridgelabz/UserDetails.java
@@ -5,18 +5,42 @@ import java.util.regex.Pattern;
 
 public class UserDetails {
 	public String input;
-
-	public boolean lastName(String lastName) {
+	public boolean firstName(String firstName) {
 		input = "([A-Z]{1}[a-z]{2,})";
 		Pattern p = Pattern.compile(input);
-		Matcher m = p.matcher(lastName);
-		if (m.matches()) {
+		Matcher m = p.matcher(firstName);
+		if (m.matches()==true) {
 			System.out.println("Valid Input");
 		} else {
 			System.out.println("Sorry!! InValid Input");
 			System.out.println("Try valid Input");
 		}
-		return false;
+		return m.matches();
+	}
+	public boolean lastName(String lastName) {
+		input = "([A-Z]{1}[a-z]{2,})";
+		Pattern p = Pattern.compile(input);
+		Matcher m = p.matcher(lastName);
+		if (m.matches()==true) {
+			System.out.println("Valid Input");
+		} else {
+			System.out.println("Sorry!! InValid Input");
+			System.out.println("Try valid Input");
+		}
+		return m.matches();
+	}
+	public boolean email(String email) {
+		input = "^[a-z0-9]+([_+-.][0-9a-z]+)*@[a-z]+.[a-z]{2,3}$";
 
+		// Pattern p = Pattern.complie("((?=.*\\d).{8,20})");
+		Pattern p = Pattern.compile(input);
+		Matcher m = p.matcher(email);
+		if (m.matches()==true) {
+			System.out.println("Valid Input");
+		} else {
+			System.out.println("Sorry!! InValid Input");
+			System.out.println("Try valid Input");
+		}
+		return m.matches();
 	}
 }
--- src/test/java/com/bridgelabz/UserDetailsTest.java
+++ src/test/java/com/bridgelabz/UserDetailsTest.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 //import org.mockito.Mock;
 
@@ -12,26 +10,66 @@ public class UserDetailsTest {
 	UserDetails details = new UserDetails();
 
 	/**
-	 * We have created this Test case to check if the first name is proper & if
+	 * We have created this Test case to check if the First name is proper & if
 	 * passes the regex pattern. The assertTrue will check if the result is true and
 	 * the it will pass the test.
 	 */
 	@Test
 	public void isValidInputFn() {
-		boolean actualResult = details.lastName("Siripurapu");
-		assertTrue(actualResult);
-
+		boolean actualResult = details.firstName("Saranya");
+		Assert.assertTrue(actualResult);
 	}
 
 	/**
 	 * We have created this test case to check if the test fails if we pass the
-	 * wrong pattern for first name The assertFalse will check if the result is
+	 * wrong pattern for First name The assertFalse will check if the result is
 	 * false and the it will pass the test.
 	 */
 	@Test
 	public void isInvalidInputFn() {
-		boolean actualResult = details.lastName("siri");
-		assertFalse(actualResult);
+		boolean actualResult1 = details.lastName("saranya");
+		Assert.assertFalse(actualResult1);
 	}
 
+	/**
+	 * We have created this Test case to check if the Last name is proper & if
+	 * passes the regex pattern. The assertTrue will check if the result is true and
+	 * the it will pass the test.
+	 */
+	@Test
+	public void isValidInputLn() {
+		boolean actualResult = details.firstName("Sir");
+		Assert.assertTrue(actualResult);
+	}
+
+	/**
+	 * We have created this test case to check if the test fails if we pass the
+	 * wrong pattern for Last name The assertFalse will check if the result is false
+	 * and the it will pass the test.
+	 */
+	@Test
+	public void isInvalidInputLn() {
+		boolean actualResult1 = details.lastName("sir");
+		Assert.assertFalse(actualResult1);
+	}
+
+	/**
+	 * Test case to check if the email is valid then result is true and the it will
+	 * pass the test.
+	 */
+	@Test
+	public void isValidEmail() {
+		boolean actualResult = details.email("acd@yahoo.co");
+		Assert.assertTrue(actualResult);
+	}
+
+	/**
+	 * Test case to check if the email is not valid then result is true and the it
+	 * will fails the test.
+	 */
+	@Test
+	public void isInvalidEmail() {
+		boolean actualResult1 = details.email("sir@@bl.co.in");
+		Assert.assertFalse(actualResult1);
+	}
 }
