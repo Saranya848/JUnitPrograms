0e4612ec7155b1dd66e15a6c9a50c5722a5e3eab
SaranyaSiripurapu <85158690+SaranyaSiripurapu@users.noreply.github.com>
2022-03-14 22:07:30 +0530

    Update UserDetailsTest.java

--- src/test/java/com/bridgelabz/UserDetailsTest.java
+++ src/test/java/com/bridgelabz/UserDetailsTest.java
@@ -1,8 +1,6 @@
 package com.bridgelabz;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 //import org.mockito.Mock;
 
@@ -10,35 +8,26 @@ public class UserDetailsTest {
 	// @Mock
 	// private UserDetails details;
 	UserDetails details = new UserDetails();
-
 	/**
-	 * We have created this Test case to check if the first name is proper & if
+	 * We have created this Test case to check if the First name is proper & if
 	 * passes the regex pattern. The assertTrue will check if the result is true and
 	 * the it will pass the test.
 	 */
 	@Test
 	public void isValidInputFn() {
 		boolean actualResult = details.firstName("Saranya");
-		assertTrue(actualResult);
-
+		Assert.assertTrue(actualResult);
 	}
 
 	/**
 	 * We have created this test case to check if the test fails if we pass the
-	 * wrong pattern for first name The assertFalse will check if the result is
-	 * false and the it will pass the test.
+	 * wrong pattern for First name The assertFalse will check if the result is false
+	 * and the it will pass the test.
 	 */
 	@Test
 	public void isInvalidInputFn() {
-		boolean actualResult = details.firstName("sary");
-		assertFalse(actualResult);
+		boolean actualResult = details.lastName("saranya");
+		Assert.assertFalse(actualResult);
 	}
-
-	//@Test
-	//public void isEqualInputFn(String firstName) {
-	//	boolean actualResult = details.firstName(firstName);
-	//	equals(actualResult);
-		// assertEqual(actualResult);
-	//}
 
 }
